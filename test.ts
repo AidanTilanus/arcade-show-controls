4f66fd98dd5215505501b95fbe5b3dd9b05bba10
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2023-12-06 18:07:25 +0100

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+forever(function () {
+    showControls.showControls(
+        showControls.createControlAction(Buttons.Arrows, "Move"),
+        showControls.createControlAction(Buttons.B, "Fireball")
+    )
+    pause(2500)
+    showControls.showControls(
+        showControls.createControlAction(Buttons.Arrows, "Move"),
+        showControls.createControlAction(Buttons.A, "Jump")
+    )
+    pause(2500)
+    showControls.showControls(
+        showControls.createControlAction(Buttons.Arrows, "Move"),
+        showControls.createControlAction(Buttons.Menu, "Invertory")
+    )
+    pause(2500)
+})
