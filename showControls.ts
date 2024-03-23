e49206f57d36ad1b3ee9408f3981f235ec798f61
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2024-03-23 15:40:02 +0100

    Update pxt.json, showControls.ts, test.ts

--- showControls.ts
+++ showControls.ts
@@ -17,48 +17,119 @@ namespace showControls {
     }
     let controlsRenderable: scene.Renderable
     
-    //% block="show controls||$firstAction$secondAction"
+    //% block="set first controls||$firstAction and second controls $secondAction"
     //% blockId="showControlsShowControls"
     //% firstAction.shadow="showControlsCreateContolAction"
     //% secondAction.shadow="showControlsCreateContolAction"
-    //% thirdAction.shadow="showControlsCreateContolAction"
     //% inlineInputMode=external
-    export function showControls(firstAction: ControlAction, secondAction?: ControlAction) {
-        controlsRenderable = scene.createRenderable(100, (screen, camera) => {
-            screen.fillRect(2, scene.screenHeight() - 11, scene.screenWidth() - 4, 11, 1)
+    export function showControls(firstActions: ControlAction[], secondActions?: ControlAction[]) {
+        firstControlActions = firstActions
+        if (secondActions) {
+            secondControlActions = secondActions
+        }
+    }
 
-            let x: number = 3
+    let firstControlActions: ControlAction[] = []
+    let secondControlActions: ControlAction[] = []
 
-            const actions: ControlAction[] = [firstAction]
-            if (secondAction) { actions.push(secondAction) }
-            
-            for (let action of actions) {
-                if (action.button == Buttons.Arrows) {
-                    spriteutils.drawTransparentImage(arrows, screen, x, scene.screenHeight() - 11)
-                    x += arrows.width + 2
-                }
-                else if (action.button == Buttons.A) {
-                    spriteutils.drawTransparentImage(aButton, screen, x, scene.screenHeight() - 11)
-                    x += aButton.width + 2
-                }
-                else if (action.button == Buttons.B) {
-                    spriteutils.drawTransparentImage(bButton, screen, x, scene.screenHeight() - 11)
-                    x += bButton.width + 2
-                }
-                else if (action.button == Buttons.Menu) {
-                    spriteutils.drawTransparentImage(menuButton, screen, x, scene.screenHeight() - 11)
-                    x += menuButton.width + 2
-                }
+    let firstControlActionsIndex: number = 0
+    let secondControlActionsIndex: number = 0
 
-                screen.print(action.action, x, scene.screenHeight() - 9, 15)
-                x += action.action.length * 6 + 1
+    let firstAction: ControlAction
+    let secondAction: ControlAction
+
+    //looping
+    forever(function() {
+        if (firstControlActions.length > 0) {
+            firstAction = firstControlActions[firstControlActionsIndex]
+            firstControlActionsIndex++
+            if (firstControlActionsIndex > firstControlActions.length - 1) {
+                firstControlActionsIndex = 0
             }
-        })
+        }
+        if (secondControlActions.length > 0) {
+            secondAction = secondControlActions[secondControlActionsIndex]
+            secondControlActionsIndex++
+            if (secondControlActionsIndex > secondControlActions.length - 1) {
+                secondControlActionsIndex = 0
+            }
+        }
+
+        renderControls()
+
+        pause(cycleTime)
+    })
+
+    //rendering
+    function renderControls() {
+        if(render) {
+            controlsRenderable = scene.createRenderable(100, (screen, camera) => {
+                screen.fillRect(2, scene.screenHeight() - 11, scene.screenWidth() - 4, 11, 1)
+
+                let x: number = 3
+
+                const actions: ControlAction[] = [firstAction]
+                if (secondAction) { actions.push(secondAction) }
+
+                for (let action of actions) {
+                    if (action.button == Buttons.Arrows) {
+                        spriteutils.drawTransparentImage(arrows, screen, x, scene.screenHeight() - 11)
+                        x += arrows.width + 2
+                    }
+                    else if (action.button == Buttons.A) {
+                        spriteutils.drawTransparentImage(aButton, screen, x, scene.screenHeight() - 11)
+                        x += aButton.width + 2
+                    }
+                    else if (action.button == Buttons.B) {
+                        spriteutils.drawTransparentImage(bButton, screen, x, scene.screenHeight() - 11)
+                        x += bButton.width + 2
+                    }
+                    else if (action.button == Buttons.Menu) {
+                        spriteutils.drawTransparentImage(menuButton, screen, x, scene.screenHeight() - 11)
+                        x += menuButton.width + 2
+                    }
+
+                    screen.print(action.action, x, scene.screenHeight() - 9, 15)
+                    x += action.action.length * 6 + 1
+                }
+            })
+        }
     }
 
     //% block="create control action for $button with action $action"
     //% blockId="showControlsCreateContolAction"
     export function createControlAction(button: Buttons, action: string): ControlAction {
         return new ControlAction(button, action)
+    }
+
+    // settings
+    let render: boolean = true
+    let cycleTime: number = 1500
+
+    //% block="show controls bar $enable"
+    //% blockId="showControlsEnableControls"
+    //% enable.shadow="toggleYesNo"
+    export function enableControls(enable: boolean) {
+        render = enable
+        renderControls()
+    }
+
+    //% block="showing controls"
+    //% blockId="showControlsGetEnabled"
+    export function getEnabled(): boolean {
+        return render
+    }
+
+    //% block="set control loop time to $amount"
+    //% blockId="showControlsSetControlLoopTime"
+    //% amount.shadow="timePicker"
+    export function setControlLoopTime(amount: number) {
+        cycleTime = amount
+    }
+
+    //% block="control loop time"
+    //% blockId="showControlsGetEnabled"
+    export function getControlLoopTime(): number {
+        return cycleTime
     }
 }
