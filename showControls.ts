3142a290fed4e7901578eec4edc27fb6ca63ad35
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2024-03-23 15:43:47 +0100

    Update showControls.ts

--- showControls.ts
+++ showControls.ts
@@ -39,61 +39,66 @@ namespace showControls {
     let secondAction: ControlAction
 
     //looping
-    forever(function() {
-        if (firstControlActions.length > 0) {
-            firstAction = firstControlActions[firstControlActionsIndex]
-            firstControlActionsIndex++
-            if (firstControlActionsIndex > firstControlActions.length - 1) {
-                firstControlActionsIndex = 0
+    forever(function() {if (render) {
+            if (firstControlActions.length > 0) {
+                        firstAction = firstControlActions[firstControlActionsIndex]
+                        firstControlActionsIndex++
+                        if (firstControlActionsIndex > firstControlActions.length - 1) {
+                            firstControlActionsIndex = 0
+                        }
+                    }
+            if (secondControlActions.length > 0) {
+                secondAction = secondControlActions[secondControlActionsIndex]
+                secondControlActionsIndex++
+                if (secondControlActionsIndex > secondControlActions.length - 1) {
+                    secondControlActionsIndex = 0
+                }
             }
-        }
-        if (secondControlActions.length > 0) {
-            secondAction = secondControlActions[secondControlActionsIndex]
-            secondControlActionsIndex++
-            if (secondControlActionsIndex > secondControlActions.length - 1) {
-                secondControlActionsIndex = 0
-            }
-        }
 
-        renderControls()
+            renderControls()
 
-        pause(cycleTime)
+            pause(cycleTime)
+        }
+            
     })
 
     //rendering
     function renderControls() {
-        if(render) {
-            controlsRenderable = scene.createRenderable(100, (screen, camera) => {
-                screen.fillRect(2, scene.screenHeight() - 11, scene.screenWidth() - 4, 11, 1)
+        if (firstControlActions.length <= 0 || secondControlActions.length <= 0) {
+            if(render) {
+                controlsRenderable = scene.createRenderable(100, (screen, camera) => {
+                    screen.fillRect(2, scene.screenHeight() - 11, scene.screenWidth() - 4, 11, 1)
 
-                let x: number = 3
+                    let x: number = 3
 
-                const actions: ControlAction[] = [firstAction]
-                if (secondAction) { actions.push(secondAction) }
+                    const actions: ControlAction[] = [firstAction]
+                    if (secondAction) { actions.push(secondAction) }
 
-                for (let action of actions) {
-                    if (action.button == Buttons.Arrows) {
-                        spriteutils.drawTransparentImage(arrows, screen, x, scene.screenHeight() - 11)
-                        x += arrows.width + 2
-                    }
-                    else if (action.button == Buttons.A) {
-                        spriteutils.drawTransparentImage(aButton, screen, x, scene.screenHeight() - 11)
-                        x += aButton.width + 2
-                    }
-                    else if (action.button == Buttons.B) {
-                        spriteutils.drawTransparentImage(bButton, screen, x, scene.screenHeight() - 11)
-                        x += bButton.width + 2
-                    }
-                    else if (action.button == Buttons.Menu) {
-                        spriteutils.drawTransparentImage(menuButton, screen, x, scene.screenHeight() - 11)
-                        x += menuButton.width + 2
-                    }
+                    for (let action of actions) {
+                        if (action.button == Buttons.Arrows) {
+                            spriteutils.drawTransparentImage(arrows, screen, x, scene.screenHeight() - 11)
+                            x += arrows.width + 2
+                        }
+                        else if (action.button == Buttons.A) {
+                            spriteutils.drawTransparentImage(aButton, screen, x, scene.screenHeight() - 11)
+                            x += aButton.width + 2
+                        }
+                        else if (action.button == Buttons.B) {
+                            spriteutils.drawTransparentImage(bButton, screen, x, scene.screenHeight() - 11)
+                            x += bButton.width + 2
+                        }
+                        else if (action.button == Buttons.Menu) {
+                            spriteutils.drawTransparentImage(menuButton, screen, x, scene.screenHeight() - 11)
+                            x += menuButton.width + 2
+                        }
 
-                    screen.print(action.action, x, scene.screenHeight() - 9, 15)
-                    x += action.action.length * 6 + 1
-                }
-            })
+                        screen.print(action.action, x, scene.screenHeight() - 9, 15)
+                        x += action.action.length * 6 + 1
+                    }
+                })
+            }
         }
+        
     }
 
     //% block="create control action for $button with action $action"
